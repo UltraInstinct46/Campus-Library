776f43d15804f3a79f18c49c54606044f35fb549
UltraInstinct46 <restuaerestu291@gmail.com>
2022-04-27 14:21:41 +0700

    First Commit

---
+++ src/campus/library/Connectionc.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package campus.library;
+import java.sql.*;
+/**
+ *
+ * @author killua
+ */
+public class Connectionc {
+    public Connection con;
+    public static void main(String[] args){
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/db_library","root","");
+            
+            if(con!=null){
+                System.out.println("successfully connected");
+                
+            }
+        } catch(Exception e){
+            System.out.println(e);
+            System.out.println("Not Connected");
+        }
+    }
+}
