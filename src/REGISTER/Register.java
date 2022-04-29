ee93803b652c0715d0c9490e8eb69e2656ae4d26
Napster230503 <alamsyahfahmi99@gmail.com>
2022-04-29 14:09:03 +0700

    Register_Update1

--- src/REGISTER/Register.java
+++ src/REGISTER/Register.java
@@ -9,5 +9,92 @@ package REGISTER;
  * @author ACER
  */
 public class Register {
+    private String name,kelas,place,date,gender;
+    private int nim;
     
+    
+    /**
+     * Return the name
+     */
+    //ENCAPSULATION
+    public String getName(){
+        return name;
+    }
+    
+    /**
+     * @param name the name to set
+     */
+    public void setName (String name){
+        this.name = name;
+    }
+    
+    /**
+     * Return the nim
+     */
+    public int getNim (){
+        return nim;
+    }
+    
+    /**
+     * @param nim the nim to set
+     */
+    public void setNim (int nim) {
+        this.nim = nim;
+    }
+    
+    /**
+     * Return the class
+     */
+    public String getKelas(){
+        return kelas;
+    }
+    
+    /**
+     * @param Kelas the Kelas to set
+     */
+    public void setKelas (String Kelas){
+        this.kelas = kelas;
+    }
+    
+    /**
+     * Return the place
+     */
+    public String getPlace(){
+        return place;
+    }
+    
+    /**
+     * @param place the place to set
+     */
+    public void setPlace (String place){
+        this.place = place;
+    }
+    
+    /**
+     * Return the date 
+     */
+    public String getDate(){
+        return date;
+    }
+    
+    /**
+     * @param date the date to set
+     */
+    public void setDate (String date){
+        this.date = date;
+    }
+    
+    /**
+     * Return gender
+     */
+    public String getGender(){
+        return gender;
+    }
+    
+    /**
+     * @param gender the gender to set
+     */
+    public void setGender (String gender){
+        this.gender = gender;
+    }
 }
